b465578530231cb69b0151cc41ccfa223f67c311
Derek Kan <derek@ischool.berkeley.edu>
2014-04-07 21:32:28 -0700

    refactored app states and ui-views. Added some basic messaging with static data services

--- content.js
+++ content.js
@@ -34,8 +34,8 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 var sidebarApp = angular.module('sidebarDatingExt',[
     'ui.router',
 	'ngRoute',
-	'profileControllers',
-	'profileServices'
+	'appControllers',
+	'appServices'
 ]);
 
 
@@ -51,113 +51,175 @@ sidebarApp.config(['$urlRouterProvider', '$sceDelegateProvider', '$stateProvider
 	    'chrome-extension://*/partials/**'
 	  ]); 
 
-    $urlRouterProvider.otherwise('/');
+    //$urlRouterProvider.otherwise('/');
 
     $stateProvider
         
         // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/',
-            views: {
-	            'profileList': {
-	            	templateUrl: chrome.extension.getURL('partials/profileList.html'),
-	            	controller: 'ProfileListCtrl'
+        .state('main', {
+	        views: {   
+	        	'sidebar': {
+	            	templateUrl: chrome.extension.getURL('partials/danceCardMenu.html'),
+	            	controller: 'DanceCardCtrl'
 	            },
-	            'profileDetail': {
+	            'detailsPanel': {
 	            	templateUrl: chrome.extension.getURL('partials/profileDetails.html'),
 	            	controller: 'ProfileListCtrl'
-	            },
-	            'danceCardMenu': {
-	            	templateUrl: chrome.extension.getURL('partials/danceCardMenu.html'),
-	            	controller: ''
 	            }
 	        }
         })
-        
-        // nested list with custom controller
-        // .state('home.list', {
-        //     url: '/list',
-        //     templateUrl: 'partial-home-list.html',
-        //     controller: function($scope) {
-        //         $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-        //     }
-        // })
-        
-        // // nested list with just some random string data
-        // .state('home.paragraph', {
-        //     url: '/paragraph',
-        //     template: 'I could sure use a drink right now.'
-        // })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('detail', {
-            url: '/',
-            controller: 'UserProfileCtrl',
-            templateUrl: chrome.extension.getURL('partials/profileDetails.html'),
+
+        .state('main.profileList', {
+        	url: '',
+			templateUrl: chrome.extension.getURL('partials/profileList.html'),
+    		controller: 'ProfileListCtrl'
+        })
+        .state('main.messages', {
+        	url: '',
+			templateUrl: chrome.extension.getURL('partials/messages.html'),
+    		controller: 'MessageCtrl'
         })
 
-	 	.state('self', {
-            url: '/',
-            controller: 'UserProfileCtrl',
-            templateUrl: chrome.extension.getURL('partials/profileSelf.html'),
+	 	.state('main.sidebar', {
+            url: '',
+	       	controller: '',
+	        templateUrl: chrome.extension.getURL('partials/profileSelf.html')
         });
   }]);
 
-// var routerApp = angular.module('routerApp', ['ui.router']);
-
-// routerApp.config(function($stateProvider, $urlRouterProvider) {
-    
-//     $urlRouterProvider.otherwise('/home');
-    
-//     $stateProvider
-        
-//         // HOME STATES AND NESTED VIEWS ========================================
-//         .state('home', {
-//             url: '/home',
-//             templateUrl: 'partial-home.html'
-//         })
-        
-//         // nested list with custom controller
-//         .state('home.list', {
-//             url: '/list',
-//             templateUrl: 'partial-home-list.html',
-//             controller: function($scope) {
-//                 $scope.dogs = ['Bernese', 'Husky', 'Goldendoodle'];
-//             }
-//         })
-        
-//         // nested list with just some random string data
-//         .state('home.paragraph', {
-//             url: '/paragraph',
-//             template: 'I could sure use a drink right now.'
-//         })
-        
-//         // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-//         .state('about', {
-//             url: '/about',
-//             views: {
-//                 '': { templateUrl: 'partial-about.html' },
-//                 'columnOne@about': { template: 'Look I am a column!' },
-//                 'columnTwo@about': { 
-//                     templateUrl: 'table-data.html',
-//                     controller: 'scotchController'
-//                 }
-//             }
-            
-//         });
-        
-// });
-
 /* Services */
 
-var profileServices = angular.module('profileServices', ['ngResource']);
+var appServices = angular.module('appServices', ['ngResource']);
 
-profileServices.factory('UiState', function(){
-	return {};
+appServices.factory('UiState', function(){
+	var uiStateService = {};
+
+	uiStateService.isSelected = -1;
+	uiStateService.selectedProfile = false;
+	uiStateService.showSidebar = true;
+	uiStateService.showDetailsPanel = false;
+
+	return uiStateService;
+});
+
+appServices.factory('MessageService', function(){
+	
+	var messageService = {};
+
+	messageService.messageData = [
+			{
+				"userId": 15,
+				"conversation": [
+			        {
+			          "senderId": 0,
+			          "message": "hello!",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 15,
+			          "message": "hi",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 15,
+			          "message": "how are you? Did you have a good day?",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 0,
+			          "message": "Yeah it was great. Do you want to get some coffee?",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 15,
+			          "message": "sure, Philz on 18th street?",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 0,
+			          "message": "sounds good, see you then",
+			          "timeStamp": "2 days ago"
+			        }
+		        ]
+		    },
+		    {
+		    	"userId": 25,
+		        "conversation": [
+		        	{
+			          "senderId": 0,
+			          "message": "hello! Sarah",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 25,
+			          "message": "hi",
+			          "timeStamp": "2 days ago"
+			        }
+		        ]
+		    },
+		    {
+		    	"userId": 35,
+		    	"conversation": [
+		        	{
+			          "senderId": 0,
+			          "message": "hello! Phillis",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 35,
+			          "message": "hi",
+			          "timeStamp": "2 days ago"
+			        }
+		        ]
+		    },
+		    {
+		    	"userId": 45,
+		    	"conversation": [
+		        	{
+			          "senderId": 0,
+			          "message": "hello! Patty",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 45,
+			          "message": "hi",
+			          "timeStamp": "2 days ago"
+			        }
+		        ]
+		    },
+		    {
+		    	"userId": 55,
+		        "conversation": [
+		        	{
+			          "senderId": 0,
+			          "message": "hello! Tilly",
+			          "timeStamp": "2 days ago"
+			        },
+			        {
+			          "senderId": 55,
+			          "message": "hi",
+			          "timeStamp": "2 days ago"
+			        }
+		        ]
+		    }
+	    ];
+
+	    messageService.getMessageByUserId = function(userId){
+	    	console.log('in Message service');
+	    	console.log(userId);
+	    	for(var i=0; i<this.messageData.length; i++){
+	    		if(userId == this.messageData[i].userId){
+	    			return this.messageData[i].conversation;
+	    		}
+	    	}
+	    	return -1;
+	    }
+
+	return messageService;
 });
 
 
-profileServices.factory('Profile', ['$resource',
+appServices.factory('Profile', ['$resource',
   function($resource){
 
   	var profileFactory = {}
@@ -172,54 +234,180 @@ profileServices.factory('Profile', ['$resource',
 
 /* Controllers */
 
-var profileControllers = angular.module('profileControllers', []);
+var appControllers = angular.module('appControllers', []);
 
-profileControllers.controller('UserProfileCtrl', ['$scope', 
-	function($scope) {
+appControllers.controller('MessageCtrl', ['$scope', 'UiState', 'MessageService', '$location', '$anchorScroll',
+	function($scope, UiState, MessageService, $location, $anchorScroll) {
+
+		$scope.messageThreads = MessageService.messageData;
+		//$scope.messageThread = MessageService.selectedConversation;//MessageService.getMessageByUserId(UiState.selectedProfile.userId);
+		$scope.messageThread = $scope.conversation;//inherited from DanceCardCtrl
+
+		$scope.newMessage;
+
+        $scope.ifSentByUser = function(i){
+        	if($scope.conversation[i].senderId == 0){
+
+        		return true;
+        	}
+        	else{
+        		return false;
+        	}
+        }
+
+        $scope.sendMessage = function(){
+
+        	if($scope.newMessage){
+	        	var message = {
+	        		senderId: 0,
+	        		message: $scope.newMessage,
+	        		timeStamp: "just now"
+	        	}
+
+	        	$scope.conversation.push(message);
+	        	$scope.newMessage = "";
+	        	//$scope.gotoBottom(); reloads and causes extension to close
+	        }
+        }
+
+        $scope.gotoBottom = function (){
+		    // set the location.hash to the id of
+		    // the element you wish to scroll to.
+		    $location.hash('bottom');
+		 
+		    // call $anchorScroll()
+		    $anchorScroll();
+		};
 
 	}]);
 
-profileControllers.controller('ProfileListCtrl', ['$scope', 'Profile', 'UiState',
+appControllers.controller('DanceCardCtrl', ['$scope', '$state', 'UiState', 'MessageService',
+	function($scope, $state, UiState, MessageService) {
+		$scope.danceCard = [
+			{
+				"userId": 15,
+				"name": "Jane",
+				"age": 27,
+				"location_city": "Oakland",
+				"location_state": "CA",
+				"personal_blurb": "I love eating and traveling. Also I like my men perfect...",
+				"relevance": 1,
+				"imageUrl": "http://lorempixel.com/36/36/people/9/"
+			},
+			{
+				"userId": 25,
+				"name": "Sarah",
+				"age": 27,
+				"imageUrl": "http://lorempixel.com/36/36/people/9/",
+				"location_city": "Oakland",
+				"location_state": "CA",
+				"personal_blurb": "Perhaps I'm unlovable, but maybe not!...",
+				"relevance": 1
+			},
+			{
+				"userId": 35,
+				"name": "Phillis",
+				"age": 27,
+				"imageUrl": "http://lorempixel.com/36/36/people/9/",
+				"location_city": "Oakland",
+				"location_state": "CA",
+				"personal_blurb": "I make ice cream every night, do you want some?..",
+				"relevance": 1
+			},
+			{
+				"userId": 45,
+				"name": "Patty",
+				"age": 27,
+				"imageUrl": "http://lorempixel.com/36/36/people/9/",
+				"location_city": "Oakland",
+				"location_state": "CA",
+				"personal_blurb": "I'm interested in web design :/...",
+				"relevance": 1
+			},
+			{
+				"userId": 55,
+				"name": "Tilly",
+				"age": 27,
+				"imageUrl": "http://lorempixel.com/36/36/people/9/",
+				"location_city": "Oakland",
+				"location_state": "CA",
+				"personal_blurb": "I love to tap dance on stage where everyone can see my moves...",
+				"relevance": 1
+			}
+		];
+
+		$scope.selectOnly = function(i){
+	    	UiState.selectedProfile = $scope.danceCard[i];
+	    	$scope.selectedProfile = $scope.danceCard[i];
+	    	$scope.showShortProfile = true;
+	    	$state.go('main.messages');
+	    	//MessageService.getMessageByUserId(UiState.selectedProfile.userId);
+	    	getConversation(UiState.selectedProfile.userId);
+	    }
+
+	    var getConversation = function(userId){
+	    	$scope.conversation = MessageService.getMessageByUserId(UiState.selectedProfile.userId);
+	    	console.log($scope.conversation);
+	    }
+
+	    $scope.showDetailedProfile = function(){
+	    	UiState.isSelected = $scope.selectedProfile.userId; 
+	    	UiState.showDetailsPanel = true;
+	    }
+
+	    $scope.ifSelected = function(){
+    	
+	    	if(UiState.selectedProfile.userId == $scope.selectedProfile.userId && $scope.selectedProfile != -1){
+	    		return true;
+	    	}
+	    	else{
+	    		return false;
+	    	}
+	    }
+
+	    $scope.conversation = false;
+		$scope.showShortProfile = false;
+		$scope.selectedProfile = -1;
+	}]);
+
+appControllers.controller('ProfileListCtrl', ['$scope', 'Profile', 'UiState',
   function($scope, Profile, UiState) {
 
     $scope.profiles = Profile.query();
     //console.log($scope.profiles);
 
-    $scope.doSomething = function(i){
-    	console.log('click happened: ' );
-    	console.log(i);
-    }
-
     $scope.selectOnly = function(i){
-    	console.log('click happened: ' );
-    	console.log(i);
-    	console.log($scope.uiState.isSelected);
-    	$scope.uiState.isSelected = i;
-    	$scope.uiState.selectedProfile = $scope.profiles[i];
+    	UiState.showDetailsPanel = true;
+    	UiState.selectedProfile = $scope.profiles[i];
     }
 
+    $scope.ifSelected = function(i){
+    	
+    	if(UiState.showDetailsPanel && UiState.selectedProfile.userId == $scope.profiles[i].userId){
+    		return true;
+    	}
+    	else{
+    		return false;
+    	}
+    }
 
     $scope.orderProp = 'relevance';
 
-    $scope.uiState = UiState;
-    $scope.uiState.isSelected = -1;
   }]);
 
-profileControllers.controller('uiCtrl', ['$scope', '$sce', 'UiState', 
-	function($scope, $sce, UiState){
+appControllers.controller('uiCtrl', ['$scope', 'UiState', 
+	function($scope, UiState){
 		var arrowLeftIconURL = chrome.extension.getURL("icons/icon_22996/icon_22996.png");
 		var arrowRightIconURL = chrome.extension.getURL("icons/icon_22997/icon_22997.png");
 
 		$scope.uiState = UiState;
 		$scope.uiState.showSidebar = true;
-		$scope.uiState.tabIconUrl = arrowRightIconURL;
-
 
 		$scope.tabAction = function(){
 			console.log($scope.uiState.tabIconUrl);
 			if($scope.uiState.showSidebar){
-				if($scope.uiState.isSelected != -1){
-					$scope.uiState.isSelected = -1;
+				if($scope.uiState.showDetailsPanel){
+					$scope.uiState.showDetailsPanel = false;
 				}
 				else{
 					$scope.uiState.showSidebar = false;
@@ -235,7 +423,7 @@ profileControllers.controller('uiCtrl', ['$scope', '$sce', 'UiState',
 		}
 	}]);
 
-profileControllers.controller('ProfileDetailCtrl', ['$scope', '$stateParams', 'Profile',
+appControllers.controller('ProfileDetailCtrl', ['$scope', '$stateParams', 'Profile',
   function($scope, $stateParams, Profile) {
     // $scope.profile = Profile.get({profileId: $routeParams.profileId}, function(profile) {
     //   $scope.mainImageUrl = profile.images[0];
